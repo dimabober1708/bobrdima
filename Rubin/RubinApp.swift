98c56f50eaffd0ea22e9f38a1c8c807ad77f07b2
DMYTRO BOBROVNYK <dimabober1708@icloud.com>
2025-12-17 18:39:57 +0200

    Add files via upload

---
+++ Rubin/RubinApp.swift
@@ -0,0 +1,17 @@
+//
+//  RubinApp.swift
+//  Rubin
+//
+//  Created by Садыг Садыгов on 16.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct RubinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
