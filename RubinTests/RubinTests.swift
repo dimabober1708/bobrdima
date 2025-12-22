8f842fa9554612415a1048b43e37180b8ad1b470
DMYTRO BOBROVNYK <dimabober1708@icloud.com>
2025-12-22 18:36:33 +0200

    Add files via upload

---
+++ RubinTests/RubinTests.swift
@@ -0,0 +1,16 @@
+//
+//  RubinTests.swift
+//  RubinTests
+//
+//  Created by Садыг Садыгов on 16.12.2025.
+//
+
+import Testing
+
+struct RubinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
